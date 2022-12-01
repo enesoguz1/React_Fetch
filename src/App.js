6a4b82f1f9b87cc6c7eecc70ed28971646bcd475
Enes Oğuz <enesoguz10@gmail.com>
2022-12-01 11:10:15 +0300

    Fetch

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Fetch from './Fetch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetch <hr /> 
+        useEffect - useState
+      </h1>
+      <Fetch></Fetch>
+    </div>
+  );
+}
+
+export default App;
